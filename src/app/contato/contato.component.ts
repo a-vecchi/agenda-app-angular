84b19228ead4b0835540065b6da5a9c3a5e740d1
“a-vecchi” <alexandre_v@yahoo.com>
2021-07-21 23:07:42 -0300

    Criando projeto agenda

---
+++ src/app/contato/contato.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+import { ContatoService } from '../contato.service';
+import { Contato } from './contato';
+
+@Component({
+  selector: 'app-contato',
+  templateUrl: './contato.component.html',
+  styleUrls: ['./contato.component.css']
+})
+
+export class ContatoComponent implements OnInit {
+
+  formulario: FormGroup;
+  contatos: Contato[] = [];
+
+  constructor(
+    private service: ContatoService,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.formulario = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  submit() {
+    console.log(this.formulario.value);
+
+    const formValues = this.formulario.value;
+    const contato: Contato = new Contato(formValues.nome, formValues.email);
+
+    this.service.save(contato).subscribe(resposta => {
+      this.contatos.push(resposta);
+      console.log(resposta);
+      console.log(this.contatos);
+    })
+  }
+
+}
